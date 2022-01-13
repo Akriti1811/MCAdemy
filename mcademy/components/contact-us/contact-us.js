c214872706457b066db8c2cfb2681120cb388d10
PIYUSH956 <piyushjaiswal956@gmail.com>
2022-01-13 14:57:48 +0530

    Contact Us Dynamic DOne

--- mcademy/components/contact-us/contact-us.js
+++ mcademy/components/contact-us/contact-us.js
@@ -1,7 +1,62 @@
 
 import classes from './ContactUs.module.css';
+import {useState} from 'react';
 
 function ContactUsComponent() {
+
+
+    const [name,setName] = useState();
+    const [email,setEmail] = useState();
+    const [number,setNumber] = useState();
+    const [message,setMessage] = useState();
+    const [contactText,setContactText] = useState("Contact Us");
+
+
+    async function InsertToDb() {
+
+       
+
+        if(name.length != 0 && email.length != 0 && number.length == 10 && message.length != 0 )
+        {
+           const contactData = {name:name,email:email,number:number,message:message}
+          
+           const json = JSON.stringify(contactData);
+           console.log(json);
+           
+           
+         const response = await fetch('/api/contactus',{
+             method:'POST',
+             body:json,
+             headers:{
+                 'Content-Type':'application/json'
+             }
+         });
+     
+         const data  = await response.json();
+         
+         if(data.acknowledged)
+         {
+              setContactText("Request Added");
+         }
+         else{
+             setContactText("Request Adding Failed")
+         }
+ 
+        
+        
+         
+     }
+     else{
+          window.alert("All field must contain Right value");
+            
+        }
+     }
+ 
+        
+
+
+
+
 
     return(
         <>
@@ -9,21 +64,21 @@ function ContactUsComponent() {
             <div className={classes.colnine}>
 
              <div className={classes.marg}>
-             <input className={classes.input} type="text"  placeholder="Name"/>
+             <input className={classes.input} type="text"  placeholder="Name" onChange={(e) => setName(e.target.value)}/>
              </div>
              <div className={classes.marg}>
-             <input className={classes.input} type="number"  placeholder="Mobile Number"/>
+             <input className={classes.input} type="number"  placeholder="Mobile Number" onChange={(e) => setNumber(e.target.value)} />
              </div>
              <div className={classes.marg}> 
-             <input className={classes.input} type="email"  placeholder="Email"/>
+             <input className={classes.input} type="email"  placeholder="Email" onChange={(e) => setEmail(e.target.value)}/>
              </div>
              <div className={classes.marg}>
-             <input  className={classes.input} type="text"  placeholder="I want to know about"/>
+             <input  className={classes.input} type="text"  placeholder="I want to know about" onChange={(e) => setMessage(e.target.value)} />
              </div>
                
             </div>
             <div className={classes.coltwo}>
-                <button className={classes.button}> Contact Us </button>
+                <button className={classes.button} onClick={InsertToDb}>{contactText} </button>
             </div>
 
         </div>
--- mcademy/pages/api/contactus.js
+++ mcademy/pages/api/contactus.js
@@ -1,14 +1,15 @@
 import { MongoClient } from "mongodb";
 import Connection from "./connection";
 export default async function Handler(req, res) {
+
   if (req.method == "POST") {
     const check = await Connection();
-    const queData = req.body;
-    console.log(req.body);
+    const contactData = req.body;
+    console.log(contactData);
 
     const db = check.db();
-    const data = db.collection("questions");
-    const result = await data.insertOne(queData);
+    const data = db.collection("contact");
+    const result = await data.insertOne(contactData);
     console.log(result);
     check.close();
 
